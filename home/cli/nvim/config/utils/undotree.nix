21ae9e91a2cd4c0f109446076d223b375a72f19c
Verantor <verantor@protonmail.com>
2024-06-04 19:56:38 +0200

    NixOS Rebuilt

---
+++ home/cli/nvim/config/utils/undotree.nix
@@ -0,0 +1,25 @@
+{ config
+, pkgs
+, host
+, user
+, ...
+}: {
+  plugins.undotree = {
+    enable = true;
+    settings = {
+      autoOpenDiff = true;
+      focusOnToggle = true;
+    };
+  };
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>ut";
+      action = "<cmd>UndotreeToggle<CR>";
+      options = {
+        silent = true;
+        desc = "Undotree";
+      };
+    }
+  ];
+}
