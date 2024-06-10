aa3c89eaaebebea792905c94cce86241e686cb38
Verantor <verantor@protonmail.com>
2024-06-10 21:55:21 +0200

    NixOS Rebuilt

--- hosts/main/default.nix
+++ hosts/main/default.nix
@@ -33,7 +33,7 @@
     theme = "catppuccin-mocha";
     logo = "${pkgs.nixos-icons}/share/icons/hicolor/48x48/apps/nix-snowflake-white.png";
   };
-
+  programs.command-not-found.enable = false;
   services.mullvad-vpn.enable = true;
   services.flatpak.enable = true;
 
