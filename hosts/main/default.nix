f6b6c01935b4dc6ec5f99dbe5df7a2553805a850
Verantor <verantor@protonmail.com>
2024-06-11 13:00:12 +0200

    NixOS Rebuilt

--- hosts/main/default.nix
+++ hosts/main/default.nix
@@ -58,8 +58,8 @@
 
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
+  #services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.plasma6.enable = true;
   xdg.portal = {
     enable = true;
     extraPortals = with pkgs; [
