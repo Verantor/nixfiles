84a5e08f444a21887370830d3f9a74386b2ee701
Verantor <verantor@protonmail.com>
2024-06-11 12:54:50 +0200

    NixOS Rebuilt

--- home/desktop/themeing.nix
+++ home/desktop/themeing.nix
@@ -20,17 +20,17 @@
   # package to use
   qt.style.package = pkgs.adwaita-qt;
 
-  gtk.enable = true;
-
-  gtk.cursorTheme.package = pkgs.bibata-cursors;
-  gtk.cursorTheme.name = "Bibata-Modern-Ice";
-
-  gtk.theme.package = pkgs.adw-gtk3;
-  gtk.theme.name = "adw-gtk3-dark";
-
-  gtk.iconTheme.package = pkgs.papirus-icon-theme;
-  gtk.iconTheme.name = "Papirus-Dark";
+  # gtk.enable = true;
   #
+  # gtk.cursorTheme.package = pkgs.bibata-cursors;
+  # gtk.cursorTheme.name = "Bibata-Modern-Ice";
+  #
+  # gtk.theme.package = pkgs.adw-gtk3;
+  # gtk.theme.name = "adw-gtk3-dark";
+  #
+  # gtk.iconTheme.package = pkgs.papirus-icon-theme;
+  # gtk.iconTheme.name = "Papirus-Dark";
+  # #
   # gtk.gtk3.bookmarks =
   #   let
   #     home = config.home.homeDirectory;
