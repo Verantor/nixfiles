b688cd92774bf3d3fc1f79742191dc959fc8b423
Verantor <verantor@protonmail.com>
2024-06-13 13:02:25 +0200

    NixOS Rebuilt

--- home/desktop/wm/dunst.nix
+++ home/desktop/wm/dunst.nix
@@ -10,25 +10,25 @@
       name = "Papirus";
 
     };
-    settings = {
-      global = {
-        frame_color = "#89b4fa";
-        separator_color = "frame";
+    # settings = {
+    #   global = {
+    #     frame_color = "#89b4fa";
+    #     separator_color = "frame";
 
-      };
-      urgency_low = {
-        background = "#1e1e2e";
-        foreground = "#cdd6f4";
-      };
-      urgency_normal = {
-        background = "#1e1e2e";
-        foreground = "#cdd6f4";
-      };
-      urgency_critical = {
-        background = "#1e1e2e";
-        foreground = "#cdd6f4";
-        frame_color = "#fab387";
-      };
-    };
+    #   };
+    #   urgency_low = {
+    #     background = "#1e1e2e";
+    #     foreground = "#cdd6f4";
+    #   };
+    #   urgency_normal = {
+    #     background = "#1e1e2e";
+    #     foreground = "#cdd6f4";
+    #   };
+    #   urgency_critical = {
+    #     background = "#1e1e2e";
+    #     foreground = "#cdd6f4";
+    #     frame_color = "#fab387";
+    #   };
+    # };
   };
 }
