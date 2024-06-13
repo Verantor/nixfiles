d7680fa06b7d23896051c73f118c1e84a6377ae6
Verantor <verantor@protonmail.com>
2024-06-13 12:52:50 +0200

    NixOS Rebuilt

--- theme/default.nix
+++ theme/default.nix
@@ -1,26 +1,50 @@
-{ ... }: {
+{ pkgs, ... }: {
   # convert rrggbb hex to #rrggbb
+  stylix = {
+    enable = true;
+    base16Scheme = {
+      base00 = "282828";
+      base01 = "3c3836";
+      base02 = "504945";
+      base03 = "665c54";
+      base04 = "bdae93";
+      base05 = "d5c4a1";
+      base06 = "ebdbb2";
+      base07 = "fbf1c7";
+      base08 = "fb4934";
+      base09 = "fe8019";
+      base0A = "fabd2f";
+      base0B = "b8bb26";
+      base0C = "8ec07c";
+      base0D = "83a598";
+      base0E = "d3869b";
+      base0F = "d65d0e";
+    };
+    image = ./wallpapers/wall2.png;
+    fonts = {
+      serif = {
+        package = pkgs.cantarell-fonts;
+        name = "Caranarell Serif";
+      };
 
+      sansSerif = {
+        package = pkgs.cantarell-fonts;
+        name = "Caranarell Sans";
+      };
 
-  stylix.base16Scheme = {
-    base00 = "282828";
-    base01 = "3c3836";
-    base02 = "504945";
-    base03 = "665c54";
-    base04 = "bdae93";
-    base05 = "d5c4a1";
-    base06 = "ebdbb2";
-    base07 = "fbf1c7";
-    base08 = "fb4934";
-    base09 = "fe8019";
-    base0A = "fabd2f";
-    base0B = "b8bb26";
-    base0C = "8ec07c";
-    base0D = "83a598";
-    base0E = "d3869b";
-    base0F = "d65d0e";
+      monospace = {
+        package = pkgs.jetbrains-mono;
+        name = "Jetbrains Mono";
+      };
+
+      emoji = {
+        package = pkgs.noto-fonts-color-emoji;
+        name = "Noto Color Emoji";
+      };
+    };
   };
-  stylix.image = ./wallpapers/wall2.png;
+
+
 
   x = c: "#${c}";
 
@@ -57,5 +81,5 @@
 
   wallpaper = ./wallpapers;
 
-  font = "FiraCode Nerd Font Mono"; #"JetBrainsMono Nerd Font Mono";
+  font = "JetBrainsMono Nerd Font Mono";
 }
