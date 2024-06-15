2b36b5ade791f035b6e326715657463eb510e06b
Verantor <verantor@protonmail.com>
2024-06-15 18:38:27 +0200

    NixOS Rebuilt

--- home/desktop/wm/lock.nix
+++ home/desktop/wm/lock.nix
@@ -35,7 +35,7 @@
           inner_color = "rgb(91, 96, 120)";
           outer_color = "rgb(24, 25, 38)";
           outline_thickness = 5;
-          placeholder_text = ''<span font_family="${theme.font}" foreground="#f2d5cf">Password...</span>'';
+          placeholder_text = ''<span font_family="${theme.font}" foreground="##f2d5cf">Password...</span>'';
           shadow_passes = 2;
         }
       ];
