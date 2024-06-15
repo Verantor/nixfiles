7e6d5f6e38757b7878f1c849d94fec6773faf2a7
Verantor <verantor@protonmail.com>
2024-06-15 18:33:28 +0200

    NixOS Rebuilt

--- home/desktop/wm/hyprland/binds.nix
+++ home/desktop/wm/hyprland/binds.nix
@@ -62,14 +62,14 @@ in
 
         "${mod}, B, exec, cliphist list | fuzzel --dmenu | cliphist decode | wl-copy"
         "${mod},ESC,exec,hyprlock"
-        ",XF86AudioRaiseVolume,exec,pamixer -i 5"
-        ",XF86AudioLowerVolume,exec,pamixer -d 5"
-        ",XF86AudioMicMute,exec,pamixer --default-source -m"
-        ",XF86AudioMute,exec,pamixer -t"
-        ",XF86AudioPlay,exec,playerctl play-pause"
-        ",XF86AudioPause,exec,playerctl play-pause"
-        ",XF86AudioNext,exec,playerctl next"
-        ",XF86AudioPrev,exec,playerctl previous"
+        # ",XF86AudioRaiseVolume,exec,pamixer -i 5"
+        # ",XF86AudioLowerVolume,exec,pamixer -d 5"
+        # ",XF86AudioMicMute,exec,pamixer --default-source -m"
+        # ",XF86AudioMute,exec,pamixer -t"
+        # ",XF87AudioPlay,exec,playerctl play-pause"
+        # ",XF86AudioPause,exec,playerctl play-pause"
+        # ",XF86AudioNext,exec,playerctl next"
+        # ",XF86AudioPrev,exec,playerctl previous"
       ];
     # ++ workspaces;
 
