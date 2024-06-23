ea6945dc984e2b5b03a2707a6b418d717916feb1
Verantor <verantor@protonmail.com>
2024-06-23 14:36:37 +0200

    NixOS Rebuilt

--- system/gaming.nix
+++ system/gaming.nix
@@ -45,12 +45,24 @@
     enableRenice = true;
     settings = {
       general = {
-        softrealtime = "auto";
         renice = 10;
       };
+
+      # Warning: GPU optimisations have the potential to damage hardware
+      gpu = {
+        apply_gpu_optimisations = "accept-responsibility";
+        gpu_device = 0;
+        amd_performance_level = "high";
+      };
       custom = {
-        start = "notify-send -a 'Gamemode' 'Optimizations activated'";
-        end = "notify-send -a 'Gamemode' 'Optimizations deactivated'";
+        start = ''
+          notify-send "GameMode started"
+          sudo /root/scripts/start_game.sh
+        '';
+        end = ''
+          notify-send "GameMode stopped"
+          sudo /root/scripts/stop_game.sh
+        '';
       };
     };
   };
