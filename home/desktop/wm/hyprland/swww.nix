48fa555462268082f863b4f28164b71aa0c993fb
Verantor <verantor@protonmail.com>
2024-06-25 17:07:56 +0200

    NixOS Rebuilt

--- home/desktop/wm/hyprland/swww.nix
+++ home/desktop/wm/hyprland/swww.nix
@@ -9,12 +9,7 @@
 
     [
       swww
-      (writeShellScriptBin "select-wall.sh" ''
-        #!/bin/sh
-        NEW_WALL=$(ls ${theme.wallpaper} | wofi --dmenu --allow-images true)
-        echo ${theme.wallpaper}/$NEW_WALL
-        swww img ${theme.wallpaper}/$NEW_WALL --transition-type grow
-      '')
+
     ];
 }
     
