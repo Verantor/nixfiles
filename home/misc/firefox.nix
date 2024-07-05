5ae6afc87be22c2a73459caa7afa62fabc9db428
Verantor <verantor@protonmail.com>
2024-07-06 01:40:20 +0200

    NixOS Rebuilt

--- home/misc/firefox.nix
+++ home/misc/firefox.nix
@@ -3,7 +3,7 @@
     target = ".mozilla/firefox/default/chrome/firefox-gnome-theme";
     source = fetchTarball {
       url = "https://github.com/rafaelmardojai/firefox-gnome-theme/archive/master.tar.gz";
-      sha256 = "1vbnsjs3sy4ipa2s47fy534bhhq7qr73wvydal63n2md664pvfa5";
+      sha256 = "";
     };
   };
 
@@ -19,9 +19,8 @@
       };
       userChrome = ''
         @import "firefox-gnome-theme/userChrome.css";
-        @import "firefox-gnome-theme/theme/colors/dark.css"; 
+        @import "firefox-gnome-theme/theme/colors/dark.css";
       '';
     };
   };
-
 }
