6bf1012ac6bdd612b486b39429ada7afc4b04ab8
Verantor <verantor@protonmail.com>
2024-07-06 01:47:51 +0200

    NixOS Rebuilt

--- home/misc/firefox.nix
+++ home/misc/firefox.nix
@@ -1,9 +1,9 @@
-{ ... }: {
+{ pkgs, ... }: {
   home.file."firefox-gnome-theme" = {
     target = ".mozilla/firefox/default/chrome/firefox-gnome-theme";
     source = fetchTarball {
       url = "https://github.com/rafaelmardojai/firefox-gnome-theme/archive/master.tar.gz";
-      sha256 = "";
+      sha256 = "02q15dclr0k8lqmynzy7gbn0ccnxjglbmnb3nniibia9gi93ycfw";
     };
   };
 
@@ -13,14 +13,51 @@
       name = "Default";
       settings = {
         # For Firefox GNOME theme:
-        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-        "browser.tabs.drawInTitlebar" = true;
-        "svg.context-properties.content.enabled" = true;
+        # "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        # "browser.tabs.drawInTitlebar" = true;
+        # "svg.context-properties.content.enabled" = true;
+        "extensions.autoDisableScopes" = 0;
       };
       userChrome = ''
         @import "firefox-gnome-theme/userChrome.css";
         @import "firefox-gnome-theme/theme/colors/dark.css";
       '';
+      search = {
+        default = "Google";
+        privatDefault = "DuckDuckGo";
+        engines = {
+          "Nix Packages" = {
+            urls = [
+              {
+                template = "https://search.nixos.org/packages";
+                params = [
+                  {
+                    name = "type";
+                    value = "packages";
+                  }
+                  {
+                    name = "query";
+                    value = "{searchTerms}";
+                  }
+                ];
+              }
+            ];
+
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@np" ];
+          };
+
+          "NixOS Wiki" = {
+            urls = [{ template = "https://wiki.nixos.org/index.php?search={searchTerms}"; }];
+            iconUpdateURL = "https://wiki.nixos.org/favicon.png";
+            updateInterval = 24 * 60 * 60 * 1000; # every day
+            definedAliases = [ "@nw" ];
+          };
+
+          "Bing".metaData.hidden = true;
+          "Google".metaData.alias = "@g"; # builtin engines only support specifying one additional alias
+        };
+      };
     };
   };
 }
