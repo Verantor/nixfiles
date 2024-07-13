81cdf477bf7ab33f677a7d90fb498e2f8d7e3d51
Verantor <verantor@protonmail.com>
2024-07-13 18:34:48 +0200

    NixOS Update

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     #nix-flatpak.url = "github:gmodena/nix-flatpak/";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
     home-manager = {
       url = "github:nix-community/home-manager";
