8c90a072a11ca3fe8e0d82d557a4b1436fbcd9c1
Verantor <verantor@protonmail.com>
2024-07-13 14:53:19 +0200

    NixOS Rebuilt

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
 
   inputs = {
     #nix-flatpak.url = "github:gmodena/nix-flatpak/";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
--- system/core/nix.nix
+++ system/core/nix.nix
@@ -43,6 +43,7 @@
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
         "https://nixpkgs-unfree.cachix.org"
+        "https://ai.cachix.org"
       ];
 
       trusted-public-keys = [
@@ -50,6 +51,7 @@
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="
       ];
     };
   };
