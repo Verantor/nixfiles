f03a0c4559c2c8560004b8c03a3180cf6a940c8a
Verantor <verantor@protonmail.com>
2024-07-22 20:40:14 +0200

    NixOS Update

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
       url = "github:nix-community/lanzaboote/v0.3.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    walker.url = "github:abenz1267/walker";
+    # walker.url = "github:abenz1267/walker";
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -59,74 +59,75 @@
   #     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
   #   ];
   # };
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  } @ inputs: let
-    inherit (self) outputs;
-    core = ./system/core;
-    bootloader = ./system/core/boot.nix;
-    sunshine = ./system/sunshine.nix;
-    services = ./system/services.nix;
-    gaming = ./system/gaming.nix;
-    amd = ./system/amd.nix;
-    sops = ./system/sops.nix;
-    scripts = ./system/scripts.nix;
-    networking = ./system/core/networking.nix;
-    polkit = ./system/polkit.nix;
-    openrgb = ./system/openrgb.nix;
-    borg = ./system/borg.nix;
-    theme = ./theme/stylix.nix;
-    keyboard = ./system/keyboard.nix;
-    server = ./system/server.nix;
-    hmModule = inputs.home-manager.nixosModules.home-manager;
-    flatpak = inputs.nix-flatpak.nixosModules.nix-flatpak;
-    nixDB = inputs.nix-index-database.nixosModules.nix-index;
-    stylix = inputs.stylix.nixosModules.stylix;
-    chaotic = inputs.chaotic.nixosModules.default;
-    shared = [core sops];
+  outputs =
+    { self
+    , nixpkgs
+    , ...
+    } @ inputs:
+    let
+      inherit (self) outputs;
+      core = ./system/core;
+      bootloader = ./system/core/boot.nix;
+      sunshine = ./system/sunshine.nix;
+      services = ./system/services.nix;
+      gaming = ./system/gaming.nix;
+      amd = ./system/amd.nix;
+      sops = ./system/sops.nix;
+      scripts = ./system/scripts.nix;
+      networking = ./system/core/networking.nix;
+      polkit = ./system/polkit.nix;
+      openrgb = ./system/openrgb.nix;
+      borg = ./system/borg.nix;
+      theme = ./theme/stylix.nix;
+      keyboard = ./system/keyboard.nix;
+      server = ./system/server.nix;
+      hmModule = inputs.home-manager.nixosModules.home-manager;
+      flatpak = inputs.nix-flatpak.nixosModules.nix-flatpak;
+      nixDB = inputs.nix-index-database.nixosModules.nix-index;
+      stylix = inputs.stylix.nixosModules.stylix;
+      chaotic = inputs.chaotic.nixosModules.default;
+      shared = [ core sops ];
 
-    home-manager = {
-      useUserPackages = true;
-      useGlobalPkgs = true;
-      extraSpecialArgs = {
-        inherit inputs;
-        inherit self;
-        inherit outputs;
-      };
-      sharedModules = [
-        {
-          stylix = {
-            targets = {
-              mangohud.enable = false;
-              waybar.enable = false;
-              hyprpaper.enable = false;
-              hyprland.enable = false;
+      home-manager = {
+        useUserPackages = true;
+        useGlobalPkgs = true;
+        extraSpecialArgs = {
+          inherit inputs;
+          inherit self;
+          inherit outputs;
+        };
+        sharedModules = [
+          {
+            stylix = {
+              targets = {
+                mangohud.enable = false;
+                waybar.enable = false;
+                hyprpaper.enable = false;
+                hyprland.enable = false;
+              };
             };
-          };
-        }
-      ];
+          }
+        ];
 
-      users.ver = {
-        imports = [./home];
-        _module.args.theme = import ./theme;
+        users.ver = {
+          imports = [ ./home ];
+          _module.args.theme = import ./theme;
+        };
       };
-    };
-  in
+    in
     #rec for recursion
     {
-      overlays = import ./system/overlays.nix {inherit inputs;};
+      overlays = import ./system/overlays.nix { inherit inputs; };
       nixosConfigurations = {
         main = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
 
           modules =
             [
-              {networking.hostName = "main";}
-              ({pkgs, ...}: {
-                nixpkgs.overlays = [inputs.rust-overlay.overlays.default];
-                environment.systemPackages = [pkgs.rust-bin.stable.latest.default];
+              { networking.hostName = "main"; }
+              ({ pkgs, ... }: {
+                nixpkgs.overlays = [ inputs.rust-overlay.overlays.default ];
+                environment.systemPackages = [ pkgs.rust-bin.stable.latest.default ];
               })
               ./hosts/main
               amd #
@@ -147,10 +148,10 @@
               chaotic
               server
               flatpak
-              {inherit home-manager;} #
+              { inherit home-manager; } #
             ]
             ++ shared;
-          specialArgs = {inherit inputs outputs;};
+          specialArgs = { inherit inputs outputs; };
         };
       };
     };
