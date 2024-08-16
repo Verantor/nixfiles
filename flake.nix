91c1f5e3c804999c4ebd402c96d646bfb9f3f97c
Verantor <verantor@protonmail.com>
2024-08-16 02:05:05 +0200

    NixOS Rebuilt

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     #nix-flatpak.url = "github:gmodena/nix-flatpak/";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable?rev=#330286)";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable/#330286)";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
     home-manager = {
       url = "github:nix-community/home-manager";
