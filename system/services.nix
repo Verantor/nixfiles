a3f90daa133e835868699fa86ca8e25280a6fad5
Verantor <verantor@protonmail.com>
2024-08-18 23:28:46 +0200

    NixOS Rebuilt

--- system/services.nix
+++ system/services.nix
@@ -1,6 +1,12 @@
 { pkgs, ... }: {
   # sound.enable = true;
 
+  services.minecraft-server = {
+    enable = true;
+    eula = true;
+    jvmOpts = "-Xms4092M -Xmx4092M -XX:+UseG1GC -XX:+CMSIncrementalPacing -XX:+CMSClassUnloadingEnabled -XX:ParallelGCThreads=2 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10";
+  };
+
   hardware.pulseaudio.enable = false;
 
   security.rtkit.enable = true;
