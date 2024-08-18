ecaa328bfcdaf2f479e4433bd789ce9444f10578
Verantor <verantor@protonmail.com>
2024-08-18 13:27:59 +0200

    NixOS Rebuilt

--- system/keyboard.nix
+++ system/keyboard.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }: {
-  hardware.keyboard.qmk.enable = true;
+  # hardware.keyboard.qmk.enable = true;
 
-  environment.systemPackages = with pkgs; [
-    via
-  ];
-  services.udev.packages = [ pkgs.via ];
+  # environment.systemPackages = with pkgs; [
+  #   via
+  # ];
+  # services.udev.packages = [ pkgs.via ];
 }
