9ea8abfccd7b96c8629eeef173566d9c861b538a
Verantor <verantor@protonmail.com>
2024-08-19 20:40:27 +0200

    NixOS Rebuilt

--- system/minecraftServer.nix
+++ system/minecraftServer.nix
@@ -1,33 +1,38 @@
 { inputs, pkgs, ... }:
 let
-  modpack = /srv/minecraft;
+  modpack = /srv/minecraft/modpack;
 in
 {
   imports = [ inputs.nix-minecraft.nixosModules.minecraft-servers ];
   nixpkgs.overlays = [ inputs.nix-minecraft.overlay ];
-  services.minecraft-servers.servers.modded-minecraft-server = {
-    enable = true;
-    autoStart = true;
+  services.minecraft-servers = {
     eula = true;
-    openFirewall = true;
-    package = pkgs.fabricServers.fabric-1_21_1;
-    dataDir = "/srv/minecraft";
-    symlinks = {
-      "mods" = "${modpack}/mods";
-    };
-    restart = "always";
-    serverProperties = {
-      server-port = 25565;
-      seed = 123456789;
-      difficulty = 3;
-      gamemode = 1;
-      max-players = 10;
-      motd = "swans in mein arsch";
-      white-list = false;
-      enable-rcon = false;
-      jvmOpts = "-Xmx4G -Xms8G";
+    enable = true;
+
+    servers.modded-minecraft-server = {
+      enable = true;
+      autoStart = true;
+      openFirewall = true;
+      package = pkgs.fabricServers.fabric-1_21_1;
+      dataDir = "/srv/minecraft";
+      symlinks = {
+        "mods" = "${modpack}/";
+      };
+      restart = "always";
+      serverProperties = {
+        server-port = 25565;
+        seed = 123456789;
+        difficulty = 3;
+        gamemode = 1;
+        max-players = 10;
+        motd = "swans in mein arsch";
+        white-list = false;
+        enable-rcon = false;
+        jvmOpts = "-Xmx4G -Xms8G";
+      };
     };
   };
+
   #connent to server with: tmux -S /run/minecraft/servername.sock attach
   #start with systemctl start minecraft-server-servername
 
