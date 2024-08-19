ce6918597d75d43de2799e6771fe29c20301cc23
Verantor <verantor@protonmail.com>
2024-08-19 21:09:07 +0200

    NixOS Rebuilt

--- system/minecraftServer.nix
+++ system/minecraftServer.nix
@@ -1,4 +1,7 @@
 { inputs, pkgs, ... }:
+let
+  modpack = /home/ver/modpack;
+in
 {
   imports = [ inputs.nix-minecraft.nixosModules.minecraft-servers ];
   nixpkgs.overlays = [ inputs.nix-minecraft.overlay ];
@@ -11,10 +14,13 @@
       autoStart = true;
       openFirewall = true;
       package = pkgs.fabricServers.fabric-1_21_1;
-
+      symlinks = {
+        "mods" = "${modpack}";
+      };
       #restart = "always";
       serverProperties = {
         server-port = 25565;
+        seed = 123456789;
         difficulty = 3;
         gamemode = 1;
         max-players = 10;
