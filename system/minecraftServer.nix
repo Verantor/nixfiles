44660eaf2e3b07bd8039fbe21375853a3e084fd0
Verantor <verantor@protonmail.com>
2024-08-20 15:03:31 +0200

    NixOS Rebuilt

--- system/minecraftServer.nix
+++ system/minecraftServer.nix
@@ -2,7 +2,7 @@
 let
   modpack = pkgs.fetchPackwizModpack {
     url = "https://github.com/Verantor/Modpack-Odd-Fabric-21/raw/1.0.3/pack.toml";
-    packHash = "sha256-e2+UTaupnkVsFh/3X41QyhUQXa+eRgKpOAcsvTlBSsw=";
+    packHash = "sha256-R78NZCVOtpWxuZ4m72gH7ohv3c972OIasiUi+fpy6zY=";
   };
 in
 {
