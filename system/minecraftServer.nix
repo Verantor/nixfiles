ba88831648b9a87fa46964657c2e3a98a8f4c0c0
Verantor <verantor@protonmail.com>
2024-08-20 14:41:18 +0200

    NixOS Rebuilt

--- system/minecraftServer.nix
+++ system/minecraftServer.nix
@@ -6,37 +6,37 @@ let
   };
 in
 {
-  environment.systemPackages = with pkgs; [
-    packwiz
-  ];
-  imports = [ inputs.nix-minecraft.nixosModules.minecraft-servers ];
-  nixpkgs.overlays = [ inputs.nix-minecraft.overlay ];
-  services.minecraft-servers = {
-    eula = true;
-    enable = true;
+  # environment.systemPackages = with pkgs; [
+  #   packwiz
+  # ];
+  # imports = [ inputs.nix-minecraft.nixosModules.minecraft-servers ];
+  # nixpkgs.overlays = [ inputs.nix-minecraft.overlay ];
+  # services.minecraft-servers = {
+  #   eula = true;
+  #   enable = true;
 
-    servers.modded-minecraft-server = {
-      enable = true;
-      #autoStart = true;
-      #openFirewall = true;
-      package = pkgs.fabricServers.fabric-1_21_1;
-      symlinks = {
-        "mods" = "${modpack}/mods";
-      };
-      restart = "no";
-      serverProperties = {
-        server-port = 25565;
-        seed = 123456789;
-        difficulty = 3;
-        gamemode = 1;
-        max-players = 10;
-        motd = "swans in mein arsch";
-        white-list = false;
-        enable-rcon = false;
-        jvmOpts = "-Xms8184M -Xmx8184M";
-      };
-    };
-  };
+  #   servers.modded-minecraft-server = {
+  #     enable = true;
+  #     #autoStart = true;
+  #     #openFirewall = true;
+  #     package = pkgs.fabricServers.fabric-1_21_1;
+  #     symlinks = {
+  #       "mods" = "${modpack}/mods";
+  #     };
+  #     restart = "no";
+  #     serverProperties = {
+  #       server-port = 25565;
+  #       seed = 123456789;
+  #       difficulty = 3;
+  #       gamemode = 1;
+  #       max-players = 10;
+  #       motd = "swans in mein arsch";
+  #       white-list = false;
+  #       enable-rcon = false;
+  #       jvmOpts = "-Xms8184M -Xmx8184M";
+  #     };
+  #   };
+  # };
 
   #connent to server with: tmux -S /run/minecraft/modded-minecraft-server.sock attach
   #start with systemctl start minecraft-server-servername
