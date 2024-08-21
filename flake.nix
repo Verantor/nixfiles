06e968d291a309e59157570e2e005bd9b49091b2
Verantor <verantor@protonmail.com>
2024-08-21 19:48:41 +0200

    NixOS Rebuilt

--- flake.nix
+++ flake.nix
@@ -80,8 +80,8 @@
       keyboard = ./system/keyboard.nix;
       server = ./system/server.nix;
       minecraftServer = ./system/minecraftServer.nix;
-      virt = ./system/virt.nix
-        hmModule = inputs.home-manager.nixosModules.home-manager;
+      virt = ./system/virt.nix;
+      hmModule = inputs.home-manager.nixosModules.home-manager;
       flatpak = inputs.nix-flatpak.nixosModules.nix-flatpak;
       nixDB = inputs.nix-index-database.nixosModules.nix-index;
       stylix = inputs.stylix.nixosModules.stylix;
