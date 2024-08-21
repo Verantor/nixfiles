cb294d142726ed628e9b98464f1ef03165e95455
Verantor <verantor@protonmail.com>
2024-08-21 18:13:18 +0200

    NixOS Rebuilt

--- home/cli/neovim/config/plugins/harpoon.nix
+++ home/cli/neovim/config/plugins/harpoon.nix
@@ -2,6 +2,28 @@
   plugins.harpoon = {
     enable = true;
     enableTelescope = true;
+    menu = {
+      borderChars = [
+        "─"
+        "│"
+        "─"
+        "│"
+        "╭"
+        "╮"
+        "╯"
+        "╰"
+      ];
+      height = 10;
+      width = 60;
+    };
+    projects = {
+      "$HOME/.dotfiled" = {
+        termCommands = [
+          "nixrb"
+          "nixup"
+        ];
+      };
+    };
     keymaps = {
       addFile = "<leader>a";
       toggleQuickMenu = "<C-e>";
--- home/cli/neovim/config/plugins/spectre.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs }: {
-  plugins.spectre = {
-    enable = true;
-    plugins.spectre.findPackage = pkgs.rg;
-    plugins.spectre.replacePackage = pkgs.gnused;
-  };
-}
