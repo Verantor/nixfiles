2df08a904bdfa5416d853b4de148dd0ab109db4a
Verantor <verantor@protonmail.com>
2024-08-21 16:29:20 +0200

    NixOS Rebuilt

--- home/cli/neovim/config/plugins/telescope.nix
+++ home/cli/neovim/config/plugins/telescope.nix
@@ -6,5 +6,14 @@
         enable = true;
       };
     };
+    keymaps = {
+      "<C-p>" = {
+        action = "git_files";
+        options = {
+          desc = "Telescope Git Files";
+        };
+      };
+      "<leader>fg" = "live_grep";
+    };
   };
 }
