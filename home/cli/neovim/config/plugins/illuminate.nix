3fedd0e5ad86a9d69d820fdb26a2851b6e127f90
Verantor <verantor@protonmail.com>
2024-08-21 18:53:27 +0200

    NixOS Rebuilt

--- home/cli/neovim/config/plugins/illuminate.nix
+++ home/cli/neovim/config/plugins/illuminate.nix
@@ -1,5 +1,16 @@
 {
   plugins.illuminate = {
     enable = true;
+    delay = 100;
+    filetypesDenylist = [
+      "dirvish"
+      "fugitive"
+    ];
+    providers = [
+      "lsp"
+      "treesitter"
+      "regex"
+    ];
+    underCursor = true;
   };
 }
