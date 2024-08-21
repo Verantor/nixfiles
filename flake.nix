7e71d174c215242055a44abb33f5906dc9b3bcce
Verantor <verantor@protonmail.com>
2024-08-21 15:40:39 +0200

    NixOS Rebuilt

--- flake.nix
+++ flake.nix
@@ -15,9 +15,9 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixvim = {
-      url = "github:nix-community/nixvim/nixos-24.05"; #
+      url = "github:nix-community/nixvim/"; #nixos-24.05
       # If using a stable channel you can use `url = "github:nix-community/nixvim/nixos-<version>"`
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
     # walker.url = "github:abenz1267/walker";
 
