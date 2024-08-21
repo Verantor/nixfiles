3ddc46a02dcf91bd3012de859ae8eb6325432ef5
Verantor <verantor@protonmail.com>
2024-08-21 16:51:25 +0200

    NixOS Rebuilt

--- home/cli/neovim/config/default.nix
+++ home/cli/neovim/config/default.nix
@@ -2,12 +2,14 @@
   # Import all your configuration modules here
   imports = [ ./plugins ];
 
-
+  globals.mapleader = " ";
   performance = {
     byteCompileLua.enable = true;
   };
   colorschemes.kanagawa = {
     enable = true;
+
   };
   luaLoader.enable = true;
+  clipboard.providers.wl-copy.enable = true;
 }
