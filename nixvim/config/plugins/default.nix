6d429dce7ef72f7371ec60851ae4f8c345d0515b
Verantor <verantor@protonmail.com>
2024-08-22 22:24:07 +0200

    NixOS Rebuilt

--- nixvim/config/plugins/default.nix
+++ nixvim/config/plugins/default.nix
@@ -1,26 +1,14 @@
-{
-  #Importallyourconfigurationmoduleshere
-  imports = [
-    ./alpha.nix
-    ./barbecue.nix
-    ./bufferline.nix
-    ./cmp.nix
-    ./commentary.nix
-    ./conform-nvim.nix
-    ./copilot-lua.nix
-    ./flash.nix
-    ./friendly-snippets.nix
-    ./harpoon.nix
-    ./illuminate.nix
-    ./indent-blankline.nix
-    ./lsp.nix
-    ./lualine.nix
-    ./luasnip.nix
-    ./neo-tree.nix
-    ./noice.nix
-    ./telescope.nix
-    ./treesitter.nix
-    ./which-key.nix
-    ./trouble.nix
-  ];
-}
+{ lib, ... }:
+let
+  definitions = lib.attrNames (
+    lib.filterAttrs
+      (
+        filename: kind:
+          filename
+          != "default.nix"
+          && (kind == "regular" || kind == "directory")
+      )
+      (builtins.readDir ./.)
+  );
+in
+lib.mkMerge (map (file: import ./${file}) definitions)
