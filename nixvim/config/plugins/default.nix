c0d6625ee18456256848209f2d4af5584f4eccd3
Verantor <verantor@protonmail.com>
2024-08-22 13:40:22 +0200

    NixOS Rebuilt

--- nixvim/config/plugins/default.nix
+++ nixvim/config/plugins/default.nix
@@ -1,31 +1,28 @@
 { pkgs, ... }:
-let
-  inherit (pkgs) lib;
-in
 {
   #Importallyourconfigurationmoduleshere
   imports = [
-    "./alpha.nix"
-    "./barbecue.nix"
-    "./bufferline.nix"
-    "./cmp.nix"
-    "./commentary.nix"
-    "./conform-nvim.nix"
-    "./copilot-lua.nix"
-    "./default.nix"
-    "./fail.nix"
-    "./flash.nix"
-    "./friendly-snippets.nix"
-    "./harpoon.nix"
-    "./illuminate.nix"
-    "./indent-blankline.nix"
-    "./lsp.nix"
-    "./lualine.nix"
-    "./luasnip.nix"
-    "./neo-tree.nix"
-    "./noice.nix"
-    "./telescope.nix"
-    "./treesitter.nix"
-    "./which-key.nix"
+    ./alpha.nix
+    ./barbecue.nix
+    ./bufferline.nix
+    ./cmp.nix
+    ./commentary.nix
+    ./conform-nvim.nix
+    ./copilot-lua.nix
+    ./default.nix
+    ./fail.nix
+    ./flash.nix
+    ./friendly-snippets.nix
+    ./harpoon.nix
+    ./illuminate.nix
+    ./indent-blankline.nix
+    ./lsp.nix
+    ./lualine.nix
+    ./luasnip.nix
+    ./neo-tree.nix
+    ./noice.nix
+    ./telescope.nix
+    ./treesitter.nix
+    ./which-key.nix
   ];
 }
