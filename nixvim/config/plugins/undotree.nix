e08c9780e11a3fb9fb5ffcf6bccfe4d08a73ff8f
Verantor <verantor@protonmail.com>
2024-08-22 22:33:41 +0200

    NixOS Rebuilt

--- nixvim/config/plugins/undotree.nix
+++ nixvim/config/plugins/undotree.nix
@@ -1,6 +1,37 @@
 {
   plugins.undotree = {
     enable = true;
-    # settings = { };
+    settings = {
+      CursorLine = true;
+      DiffAutoOpen = true;
+      DiffCommand = "diff";
+      DiffpanelHeight = 10;
+      HelpLine = true;
+      HighlightChangedText = true;
+      HighlightChangedWithSign = true;
+      HighlightSyntaxAdd = "DiffAdd";
+      HighlightSyntaxChange = "DiffChange";
+      HighlightSyntaxDel = "DiffDelete";
+      RelativeTimestamp = true;
+      SetFocusWhenToggle = true;
+      ShortIndicators = false;
+      SplitWidth = 40;
+      TreeNodeShape = "*";
+      TreeReturnShape = "\\";
+      TreeSplitShape = "/";
+      TreeVertShape = "|";
+      WindowLayout = 4;
+    };
   };
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>ut";
+      action = "<cmd>UndotreeToggle<cr>";
+      options = {
+        desc = "Open Undotree";
+      };
+    }
+  ];
 }
