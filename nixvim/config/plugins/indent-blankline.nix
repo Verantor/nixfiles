a1aa33b18605a4d030da3f00e6ce1211bde1e746
Verantor <verantor@protonmail.com>
2024-08-23 19:32:17 +0200

    NixOS Rebuilt

--- nixvim/config/plugins/indent-blankline.nix
+++ nixvim/config/plugins/indent-blankline.nix
@@ -20,9 +20,11 @@
         ];
       };
       indent = {
+        smart_indent_cap = true;
         char = "▎";
       };
       scope = {
+        enabled = true;
         show_end = true;
         show_exact_scope = true;
         show_start = true;
