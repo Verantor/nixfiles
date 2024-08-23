020bb46e6f479df401e2db592eaf9ffa90019ee2
Verantor <verantor@protonmail.com>
2024-08-23 18:23:57 +0200

    NixOS Rebuilt

--- nixvim/config/plugins/default.nix
+++ nixvim/config/plugins/default.nix
@@ -1,42 +1,41 @@
-{ lib, ... }:
-let
-  definitions = lib.attrNames (
-    lib.filterAttrs
-      (
-        filename: kind:
-          filename
-          != "default.nix"
-          && (kind == "regular" || kind == "directory")
-      )
-      (builtins.readDir ./.)
-  );
-in
-lib.mkMerge (map (file: import ./${file}) definitions)
-#{
-#  #Importallyourconfigurationmoduleshere
-#  imports = [
-#    ./alpha.nix
-#    ./bufferline.nix
-#    ./cmp.nix
-#    ./commentary.nix
-#    ./conform-nvim.nix
-#    ./copilot-lua.nix
-#    ./flash.nix
-#    ./friendly-snippets.nix
-#    ./harpoon.nix
-#    ./illuminate.nix
-#    ./indent-blankline.nix
-#    ./lsp.nix
-#    ./lualine.nix
-#    ./luasnip.nix
-#    ./neo-tree.nix
-#    ./noice.nix
-#    ./telescope.nix
-#    ./treesitter.nix
-#    ./which-key.nix
-#    ./trouble.nix
-#    ./undotree.nix
-#    ./lspsaga.nix
-#  ];
-#}
-
+# { lib, ... }:
+# let
+#   definitions = lib.attrNames (
+#     lib.filterAttrs
+#       (
+#         filename: kind:
+#           filename
+#           != "default.nix"
+#           && (kind == "regular" || kind == "directory")
+#       )
+#       (builtins.readDir ./.)
+#   );
+# in
+# lib.mkMerge (map (file: import ./${file}) definitions)
+{
+  #Importallyourconfigurationmoduleshere
+  imports = [
+    ./alpha.nix
+    ./bufferline.nix
+    ./cmp.nix
+    ./commentary.nix
+    ./conform-nvim.nix
+    ./copilot-lua.nix
+    ./flash.nix
+    ./friendly-snippets.nix
+    ./harpoon.nix
+    ./illuminate.nix
+    ./indent-blankline.nix
+    ./lsp.nix
+    ./lualine.nix
+    ./luasnip.nix
+    ./neo-tree.nix
+    ./noice.nix
+    ./telescope.nix
+    ./treesitter.nix
+    ./which-key.nix
+    ./trouble.nix
+    ./undotree.nix
+    ./gitsigns.nix
+  ];
+}
