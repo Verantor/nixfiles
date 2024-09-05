603f6e6aadabd9ce51cc5f0e0e7ef84545d3df06
Verantor <verantor@protonmail.com>
2024-09-05 17:42:57 +0200

    NixOS Update

--- nixvim/config/plugins/lsp.nix
+++ nixvim/config/plugins/lsp.nix
@@ -1,20 +1,20 @@
 {
   plugins = {
     lsp-format = {
-      enable = false; # Enable it if you want lsp-format integration for none-ls
+      enable = true; # Enable it if you want lsp-format integration for none-ls
     };
     lsp = {
       enable = true;
       capabilities = "offsetEncoding =  'utf-16'";
       servers = {
-        clangd = { enable = true; };
+        clangd = {enable = true;};
         # nixd = { enable = true; };
-        nil-ls = { enable = true; };
-        pyright = { enable = true; };
-        ruff = { enable = true; };
-        gopls = { enable = true; };
+        nil-ls = {enable = true;};
+        pyright = {enable = true;};
+        ruff = {enable = true;};
+        gopls = {enable = true;};
 
-        marksman = { enable = true; };
+        marksman = {enable = true;};
         rust-analyzer = {
           enable = true;
           installCargo = true; #TODO maby conflicts with things
