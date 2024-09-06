fc197c9db83037136c7bbb54b4db08cdc6a27744
Verantor <verantor@protonmail.com>
2024-09-06 13:52:03 +0200

    'NixOS Rebuilt'

--- pkgs/nix-orca.nix
+++ pkgs/nix-orca.nix
@@ -1,30 +1,34 @@
-{ lib
-, stdenv
-, fetchFromGitHub
-, buildGoModule
-,
-}:
-let
-  version = "0.0.8";
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  buildGoModule,
+  pkgs,
+}: let
+  version = "0.0.9";
 in
-buildGoModule {
-  pname = "nix-orca";
-  inherit version;
+  buildGoModule {
+    pname = "nix-orca";
+    inherit version;
 
-  src = fetchFromGitHub {
-    owner = "Verantor";
-    repo = "nix-orca";
-    rev = "v${version}";
-    hash = "sha256-+5Kf9YeEaOn+HUbsKEdbgJTR7S18FSvYQbHoHcx83TE=";
-    # hash = lib.fakeHash;
-  };
-
-  vendorHash = "sha256-ImkjeeMnP58F0b/0p1AvPdG7zsl69iX9IEFLXvQgPEg=";
-  # vendorHash = lib.fakeHash;
-  meta = with lib; {
-    homepage = "https://github.com/Verantor/nix-orca";
-    description = "nix helper scripts";
-    license = licenses.gpl3;
-    maintainers = with maintainers; [ Verantor ];
-  };
-}
+    src = fetchFromGitHub {
+      owner = "Verantor";
+      repo = "nix-orca";
+      rev = "v${version}";
+      hash = "sha256-B1MHDXowxOCyzdkEWkJsax/38wGmI4Fe7/WPbI37W9Q=";
+      # hash = lib.fakeHash;
+    };
+    nativeBuildInputs = [pkgs.installShellFiles];
+    postInstall = ''
+        installShellCompletion --cmd nix-orca \
+      --fish <($out/bin/nix-orca completion fish) \
+    '';
+    vendorHash = "sha256-ImkjeeMnP58F0b/0p1AvPdG7zsl69iX9IEFLXvQgPEg=";
+    # vendorHash = lib.fakeHash;
+    meta = with lib; {
+      homepage = "https://github.com/Verantor/nix-orca";
+      description = "nix helper scripts";
+      license = licenses.gpl3;
+      maintainers = with maintainers; [Verantor];
+    };
+  }
