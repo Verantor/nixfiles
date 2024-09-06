380ab136f3d04c5364a8ad5a14b522354a4328f1
Verantor <verantor@protonmail.com>
2024-09-06 16:01:21 +0200

    'NixOS Rebuilt'

--- nixvim/config/core/default.nix
+++ nixvim/config/core/default.nix
@@ -4,8 +4,38 @@
     byteCompileLua.enable = true;
     # combinePlugins.enable = true;
   };
-  colorschemes.kanagawa = {
+  colorschemes.tokyonight = {
     enable = true;
+    settings = {
+      day_brightness = 1;
+      dim_inactive = false;
+      hide_inactive_statusline = false;
+      light_style = "day";
+      lualine_bold = false;
+      on_colors = "function(colors) end";
+      on_highlights = "function(highlights, colors) end";
+      sidebars = [
+        "qf"
+        "vista_kind"
+        "terminal"
+        "packer"
+      ];
+      style = "night";
+      styles = {
+        comments = {
+          italic = true;
+        };
+        floats = "dark";
+        functions = {};
+        keywords = {
+          italic = true;
+        };
+        sidebars = "dark";
+        variables = {};
+      };
+      terminal_colors = true;
+      transparent = false;
+    };
   };
   luaLoader.enable = true;
   clipboard.providers.wl-copy.enable = true;
