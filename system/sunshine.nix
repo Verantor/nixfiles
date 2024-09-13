da7c1f5ab4158d4c0acb81aaedc5442806145f1d
Verantor <verantor@protonmail.com>
2024-09-13 14:38:06 +0200

    NixOS Rebuilt

--- system/sunshine.nix
+++ system/sunshine.nix
@@ -8,12 +8,6 @@
       sunshine_name = "nixos";
     };
   };
-  security.wrappers.sunshine = {
-    owner = "root";
-    group = "root";
-    capabilities = "cap_sys_admin+p";
-    source = "${pkgs.sunshine}/bin/sunshine";
-  };
 
   services.avahi.publish.userServices = true;
   boot.kernelModules = [ "uinput" ];
