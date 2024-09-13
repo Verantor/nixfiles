518fa3e01b8a4c54188dd9b1ed2902a420007083
Verantor <verantor@protonmail.com>
2024-09-13 17:51:21 +0200

    NixOS Rebuilt

--- home/desktop/wm/wofi/default.nix
+++ home/desktop/wm/wofi/default.nix
@@ -63,20 +63,23 @@
   programs.anyrun = {
     enable = true;
     config = {
-      plugins = [
-        inputs.anyrun.packages.${pkgs.system}.applications
-        # An array of all the plugins you want, which either can be paths to the .so files, or their packages
+      plugins = with inputs.anyrun.packages.${pkgs.system}; [
+        applications
+        # randr
+        rink
+        shell
+        symbols
       ];
       x = { fraction = 0.5; };
       y = { fraction = 0.3; };
       width = { fraction = 0.3; };
-      hideIcons = false;
-      ignoreExclusiveZones = false;
-      layer = "overlay";
+      # hideIcons = false;
+      # ignoreExclusiveZones = false;
+      # layer = "overlay";
       hidePluginInfo = false;
-      closeOnClick = false;
-      showResultsImmediately = false;
-      maxEntries = null;
+      closeOnClick = true;
+      # showResultsImmediately = false;
+      # maxEntries = null;
     };
   };
 
