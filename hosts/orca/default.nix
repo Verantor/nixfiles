94fd0fd647337a51670037e50b628fb806cf56b6
Verantor <verantor@protonmail.com>
2024-09-14 10:04:10 +0200

    'NixOS Rebuilt'

--- hosts/orca/default.nix
+++ hosts/orca/default.nix
@@ -1,15 +1,14 @@
-{ config
-, pkgs
-, lib
-, ...
-}:
-let
-  interface = "wlan0";
-in
 {
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  interface = "wlan0";
+in {
   boot = {
     kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
-    initrd.availableKernelModules = [ "xhci_pci" "usbhid" "usb_storage" ];
+    initrd.availableKernelModules = ["xhci_pci" "usbhid" "usb_storage"];
     loader = {
       grub.enable = false;
       generic-extlinux-compatible.enable = true;
@@ -19,12 +18,13 @@ in
     git
     vim
     qrencode
+    mpv
   ];
   fileSystems = {
     "/" = {
       device = "/dev/disk/by-label/NIXOS_SD";
       fsType = "ext4";
-      options = [ "noatime" ];
+      options = ["noatime"];
     };
   };
 
@@ -32,10 +32,10 @@ in
     wireless = {
       enable = true;
       # networks."${SSID}".psk = SSIDpassword;
-      interfaces = [ interface ];
+      interfaces = [interface];
     };
   };
-  sops.secrets."wireless.env" = { };
+  sops.secrets."wireless.env" = {};
   networking.wireless.environmentFile = config.sops.secrets."wireless.env".path;
   networking.wireless.networks = {
     "@home_uuid@" = {
