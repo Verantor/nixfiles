a55860436af9da0903f645cfad79286b9ab65e02
Verantor <verantor@protonmail.com>
2024-09-15 16:25:34 +0200

    Revert "NixOS Rebuilt"
    This reverts commit 298a6635e376fa8d8e19b649afdb614f041393da.

--- hosts/orca/default.nix
+++ hosts/orca/default.nix
@@ -36,13 +36,13 @@ in
       interfaces = [ interface ];
     };
   };
-  # sops.secrets."wireless.env" = { };
-  # networking.wireless.environmentFile = config.sops.secrets."wireless.env".path;
-  # networking.wireless.networks = {
-  #   "@home_uuid@" = {
-  #     psk = "@home_psk@";
-  #   };
-  # };
+  sops.secrets."wireless.env" = { };
+  networking.wireless.environmentFile = config.sops.secrets."wireless.env".path;
+  networking.wireless.networks = {
+    "@home_uuid@" = {
+      psk = "@home_psk@";
+    };
+  };
 
   hardware.enableRedistributableFirmware = true;
   system.stateVersion = "23.11";
