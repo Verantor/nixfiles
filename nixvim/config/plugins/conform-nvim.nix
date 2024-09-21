f3793c984360b0f587784757e75823d51e9125aa
Verantor <verantor@protonmail.com>
2024-09-21 17:16:57 +0200

    NixOS Rebuilt

--- nixvim/config/plugins/conform-nvim.nix
+++ nixvim/config/plugins/conform-nvim.nix
@@ -1,26 +1,28 @@
 {
   plugins.conform-nvim = {
     enable = true;
-    formatOnSave = {
-      lspFallback = true;
-      timeoutMs = 500;
-    };
-    notifyOnError = true;
-    formattersByFt = {
-      # Conform will run multiple formatters sequentially
-      python = [ "ruff" ];
-      lua = [ "stylua" ];
-      nix = [ "alejandra" ];
-      markdown = [ [ "prettierd" "prettier" ] ];
-      rust = [ "rustfmt" ];
-      go = [ "gofmt" ];
-      # Use a sub-list to run only the first available formatter
-      javascript = [ [ "prettierd" "prettier" ] ];
-      # Use the "*" filetype to run formatters on all filetypes.
-      # "*" = [ "codespell" ];
-      # Use the "_" filetype to run formatters on filetypes that don't
-      # have other formatters configured.
-      "_" = [ "trim_whitespace" ];
+    settings = {
+      format_on_save = {
+        lspFallback = true;
+        timeoutMs = 500;
+      };
+      notify_on_error = true;
+      formatters_by_ft = {
+        # Conform will run multiple formatters sequentially
+        python = [ "ruff" ];
+        lua = [ "stylua" ];
+        nix = [ "alejandra" ];
+        markdown = [ [ "prettierd" "prettier" ] ];
+        rust = [ "rustfmt" ];
+        go = [ "gofmt" ];
+        # Use a sub-list to run only the first available formatter
+        javascript = [ [ "prettierd" "prettier" ] ];
+        # Use the "*" filetype to run formatters on all filetypes.
+        # "*" = [ "codespell" ];
+        # Use the "_" filetype to run formatters on filetypes that don't
+        # have other formatters configured.
+        "_" = [ "trim_whitespace" ];
+      };
     };
   };
   keymaps = [
