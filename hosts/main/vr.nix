debea96921d2290e83d3aeec836a3b6543f9f06e
Verantor <verantor@protonmail.com>
2024-09-27 14:19:33 +0200

    NixOS Rebuilt

--- hosts/main/vr.nix
+++ hosts/main/vr.nix
@@ -13,10 +13,10 @@
   #   enable = true;
   #   defaultRuntime = true; # Register as default OpenXR runtime
   # };
-  # systemd.user.services.monado.environment = {
-  #   STEAMVR_LH_ENABLE = "1";
-  #   XRT_COMPOSITOR_COMPUTE = "1";
-  # };
+  systemd.user.services.monado.environment = {
+    STEAMVR_LH_ENABLE = "1";
+    XRT_COMPOSITOR_COMPUTE = "1";
+  };
   environment.systemPackages = with pkgs; [
     wlx-overlay-s
   ];
