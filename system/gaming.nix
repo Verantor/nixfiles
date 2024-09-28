1c3d45b485bd8a7e8a436ed606db68c01436d6b0
Verantor <verantor@protonmail.com>
2024-09-28 14:10:21 +0200

    NixOS Rebuilt

--- system/gaming.nix
+++ system/gaming.nix
@@ -54,11 +54,11 @@
       };
 
       # Warning: GPU optimisations have the potential to damage hardware
-      gpu = {
-        apply_gpu_optimisations = "accept-responsibility";
-        gpu_device = 0;
-        amd_performance_level = "high";
-      };
+      # gpu = {
+      #   apply_gpu_optimisations = "accept-responsibility";
+      #   gpu_device = 0;
+      #   amd_performance_level = "high";
+      # };
       custom = {
         start = "sudo /root/scripts/start_game.sh";
         end = "sudo /root/scripts/stop_game.sh";
