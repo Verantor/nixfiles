6d4e77cd6a148814c03c6cd6e635c6f94e4045be
Verantor <verantor@protonmail.com>
2024-09-28 12:37:58 +0200

    NixOS Rebuilt

--- system/specialisation.nix
+++ system/specialisation.nix
@@ -1,35 +1,7 @@
 { config
 , lib
 , ...
-}:
-let
-  inherit
-    (lib)
-    mkIf
-    mkEnableOption
-    mkPackageOption
-    mkOption
-    optionalString
-    optionalAttrs
-    isDerivation
-    recursiveUpdate
-    getExe
-    literalExpression
-    types
-    maintainers
-    ;
-in
-{
-  options = {
-    sys = {
-      gnome = {
-        enable = mkEnableOption "GNOME, a desktop environment";
-      };
-      hyprland = {
-        enable = mkEnableOption "Hyprland, a Wayland compositor";
-      };
-    };
-  };
+}: {
   specialisation = {
     hyprland.configuration = {
       system.nixos.tags = [ "hyprland" ];
