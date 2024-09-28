d6d3b332d9361836827d9f357b3cd9dc1f7af3a9
Verantor <verantor@protonmail.com>
2024-09-28 12:51:09 +0200

    NixOS Rebuilt

--- system/options.nix
+++ system/options.nix
@@ -19,20 +19,22 @@ in
 {
   options = {
     sys = {
-      gnome = {
-        enable = mkOption {
-          type = types.bool;
-          default = false;
-          description = "GNOME, a desktop environment";
-        };
-      };
-      hyprland = {
-        enable = mkOption {
-          type = types.bool;
-          default = true;
-          description = "Hyprland, a custom desktop environment";
-        };
-      };
+      gnome = mkEnableOption "GNOME, a desktop environment";
+      # gnome = {
+      # enable = mkOption {
+      #   type = types.bool;
+      #   default = false;
+      #   description = "GNOME, a desktop environment";
+      # };
+      # };
+      hyprland = mkEnableOption "Hyprland, a custom desktop environment";
+      # hyprland = {
+      #   enable = mkOption {
+      #     type = types.bool;
+      #     default = true;
+      #     description = "Hyprland, a custom desktop environment";
+      #   };
+      # };
     };
   };
 }
