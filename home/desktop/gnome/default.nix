01295af854d562df2bd68c8e656cdb0cf58282c7
Verantor <verantor@protonmail.com>
2024-09-28 13:50:36 +0200

    NixOS Rebuilt

--- home/desktop/gnome/default.nix
+++ home/desktop/gnome/default.nix
@@ -1,33 +1,8 @@
 { config
 , lib
 , ...
-}:
-let
-  inherit
-    (lib)
-    mkIf
-    mkEnableOption
-    mkPackageOption
-    mkOption
-    optionalString
-    optionalAttrs
-    isDerivation
-    recursiveUpdate
-    getExe
-    literalExpression
-    types
-    maintainers
-    ;
-
-  cfg = config.sys.gnome;
-in
-{
-  options = {
-    sys.gnome = {
-      enable = mkEnableOption "GNOME, a desktop environment";
-    };
-  };
-  imports = mkIf cfg.sys.gnome [
+}: {
+  imports = [
     ./gnome.nix
   ];
 }
--- home/desktop/wm/default.nix
+++ home/desktop/wm/default.nix
@@ -1,34 +1,8 @@
 { config
 , lib
 , ...
-}:
-let
-  inherit
-    (lib)
-    mkIf
-    mkEnableOption
-    mkPackageOption
-    mkOption
-    optionalString
-    optionalAttrs
-    isDerivation
-    recursiveUpdate
-    getExe
-    literalExpression
-    types
-    maintainers
-    ;
-
-  cfg = config.sys.hyprland;
-in
-{
-  options = {
-    sys.hyprland = {
-      enable = mkEnableOption "Hyprland, a Wayland compositor";
-    };
-  };
-
-  imports = mkIf cfg.enable [
+}: {
+  imports = [
     ./hyprland
     ./waybar
     ./lock.nix
