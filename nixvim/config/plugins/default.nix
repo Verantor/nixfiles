e7c930e783d5163e53c68a1893f557407f5cd3d0
Verantor <verantor@protonmail.com>
2024-10-07 21:03:25 +0200

    NixOS Rebuilt

--- nixvim/config/plugins/default.nix
+++ nixvim/config/plugins/default.nix
@@ -12,50 +12,42 @@
 #   );
 # in
 # lib.mkMerge (map (file: import ./${file}) definitions)
-{ ... }: {
-  # imports by full path without copying to /nix/store
-  imports = builtins.map (n: toString ./. + "/${n}") (builtins.attrNames (builtins.removeAttrs (builtins.readDir ./.) [ (builtins.unsafeGetAttrPos "_" { _ = null; }).file ]));
-
-  # copies all files from the current directory to /nix/store and imports from /nix/store
-  # imports = builtins.map (n: "${./.}/${n}") (builtins.attrNames (builtins.removeAttrs (builtins.readDir ./.) [(builtins.unsafeGetAttrPos "_" {_ = null;}).file]));
+{
+  #Importallyourconfigurationmoduleshere
+  imports = [
+    ./alpha.nix
+    ./barbar.nix
+    ./cmp.nix
+    ./comment.nix
+    ./conform-nvim.nix
+    ./copilot-lua.nix
+    ./flash.nix
+    ./friendly-snippets.nix
+    ./harpoon.nix
+    ./illuminate.nix
+    ./indent-blankline.nix
+    ./lsp.nix
+    ./lualine.nix
+    ./luasnip.nix
+    ./neo-tree.nix
+    ./noice.nix
+    ./telescope.nix
+    ./treesitter.nix
+    ./which-key.nix
+    ./trouble.nix
+    ./undotree.nix
+    ./gitsigns.nix
+    ./neoscroll.nix
+    ./cursorline.nix
+    ./nvim-colorizer.nix
+    ./nvim-autopairs.nix
+    ./surround.nix
+    ./rainbow-delimiters.nix
+    ./multicursors.nix
+    ./precognition.nix
+    ./yanky.nix
+    ./spectre.nix
+    ./web-devicons.nix
+    ./render-markdown.nix
+  ];
 }
-# {
-#   #Importallyourconfigurationmoduleshere
-#   imports = [
-#     ./alpha.nix
-#     ./barbar.nix
-#     ./cmp.nix
-#     ./comment.nix
-#     ./conform-nvim.nix
-#     ./copilot-lua.nix
-#     ./flash.nix
-#     ./friendly-snippets.nix
-#     ./harpoon.nix
-#     ./illuminate.nix
-#     ./indent-blankline.nix
-#     ./lsp.nix
-#     ./lualine.nix
-#     ./luasnip.nix
-#     ./neo-tree.nix
-#     ./noice.nix
-#     ./telescope.nix
-#     ./treesitter.nix
-#     ./which-key.nix
-#     ./trouble.nix
-#     ./undotree.nix
-#     ./gitsigns.nix
-#     ./neoscroll.nix
-#     ./cursorline.nix
-#     ./nvim-colorizer.nix
-#     ./nvim-autopairs.nix
-#     ./surround.nix
-#     ./rainbow-delimiters.nix
-#     ./multicursors.nix
-#     ./precognition.nix
-#     ./yanky.nix
-#     ./spectre.nix
-#     ./web-devicons.nix
-#     ./render-markdown.nix
-#   ];
-# }
-
