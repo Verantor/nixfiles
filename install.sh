c32105056e2ab041d7a9526066c10643e9374a4d
Verantor <56546640+Verantor@users.noreply.github.com>
2024-10-18 16:24:37 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,1 @@
-curl https://raw.githubusercontent.com/Verantor/nixfiles/refs/heads/main/hosts/main/disko.nix -o /tmp/disko-config.nix
-sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/disko-config.nix
-sudo nixos-generate-config --no-filesystems --root /mnt
-cd /mnt/etc/nixos/
-git clone git@github.com:Verantor/nixfiles.git
-cd nixfiles/
-
-
-sudo nix  --experimental-features "nix-command flakes" run 'github:nix-community/disko#disko-install/latest' -- --flake github.com:Verantor/nixfiles#main --disk <disk-name> <disk-device>
+sudo nix  --experimental-features "nix-command flakes" run 'github:nix-community/disko#disko-install/latest' -- --flake github.com:Verantor/nixfiles#main --disk main /dev/nvme0n1
